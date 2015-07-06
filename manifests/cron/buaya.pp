73b2d6f7faa9726aac8e9493b5f49e6eaa32da43
Mahyuddin Susanto <udienz@gmail.com>
2015-07-07 04:43:59 +0700

    add commas in cron file
    add comma to avoid errors in the future

--- manifests/cron/buaya.pp
+++ manifests/cron/buaya.pp
@@ -5,27 +5,27 @@ class buaya::cron::buaya {
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
    hour => 'absent',
-   minute => '*/15'
+   minute => '*/15',
  }
   cron { 'collect mirror size':
    command => '$SEDOT_BASE/bin/collect-mirror-size > /dev/null 2>&1',
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
    hour => '3',
-   minute => '0'
+   minute => '0',
  }
   cron { 'collect disk usage':
    command => '$SEDOT_BASE/bin/collect-du > /dev/null 2>&1',
    user => 'ftpmaster',
    hour => 'absent',
-   minute => '*/15'
+   minute => '*/15',
  }
   cron { 'plot mirror size':
    command => '$SEDOT_BASE/bin/plot-mirror-size > /dev/null 2>&1',
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
    hour => '5',
-   minute => '0'
+   minute => '0',
  }
   cron { 'update ls-lRh':
    command => '$SEDOT_BASE/bin/update-ls-lRh > /dev/null 2>&1',
@@ -40,7 +40,7 @@ class buaya::cron::buaya {
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
    hour => 'absent',
-   minute => '*/15'
+   minute => '*/15',
    ensure => absent,
  }
 
@@ -48,25 +48,25 @@ class buaya::cron::buaya {
    command => '$SEDOT_BASE/bin/create-centos-iso.sh > /dev/null 2>&1',
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
-   weekday => '0'
+   weekday => '0',
  }
   cron { 'make fedora iso':
    command => '$SEDOT_BASE/bin/create-fedora-iso.sh > /dev/null 2>&1',
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
-   weekday => '0'
+   weekday => '0',
  }
   cron { 'make opensuse iso':
    command => '$SEDOT_BASE/bin/create-opensuse-iso.sh > /dev/null 2>&1',
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
-   weekday => '0'
+   weekday => '0',
  }
   cron { 'cleaning up lock files':
    command => '$SEDOT_BASE/bin/cleanup > /dev/null 2>&1',
    environment => 'SEDOT_BASE=/home/ftpmaster/status',
    user => 'ftpmaster',
-   special => 'reboot'
+   special => 'reboot',
  }
 
 
